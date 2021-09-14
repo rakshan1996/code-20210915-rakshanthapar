a9fbe79a213084f6353f98c36c52bef29a682470
Rakshan Thapar <rakshanthapar32@gmail.com>
2021-09-15 01:02:47 +0530

    Express Server Set  Up for Implementation

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app =express();
+
+
+app.get('/fetchProcessedJson',(req,res)=>{
+
+});
+
+
+app.post('/processJson',(req,res)=>{
+
+});
+
+
+app.listen(5000,()=>{
+    console.log("listening on port 5000");
+});
